f24aeaf62f03b85816ffc0041a9b2759275704f8
alexg-rgb <garcaoalexandre@gmail.com>
2021-05-12 21:32:10 +0200

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Lexflix</title>
+</head>
+<body>
+    <div class="bg-blue-500">
+        YO
+    </div>
+</body>
+</html>
+
